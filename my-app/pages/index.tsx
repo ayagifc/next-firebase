b3670c9a67f5550216e64f51ad4b52432ca69edb
ayagifc <79235899+ayagifc@users.noreply.github.com>
2023-03-11 19:09:46 +0900

    最初のコミット

---
+++ my-app/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <form action="/send-data-here" method="post">
+          <label htmlFor="first">First name:</label>
+          <input type="text" id="first" name="first" />
+          <label htmlFor="last">Last name:</label>
+          <input type="text" id="last" name="last" />
+          <button type="submit">Submit</button>
+        </form>
+      </main>
+    </>
+  )
+}
